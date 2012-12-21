a6e1ae6a6bcd600ba32b0e25812c1e956c27dc0a
Martin Dahl <martin@b14.dk>
2012-12-21 16:19:00 +0100

    Rettet formatering af php code Rettet javascript

--- ding_periodical.js
+++ ding_periodical.js
@@ -1,13 +1,13 @@
 (function ($) {
-$(document).ready(function(){
+  $(document).ready(function(){
     $('.field-name-ding-periodical-issues li').children('.item-list').hide();
-});
-
+  });
+  
   Drupal.behaviors.dingPeriodicalIssueToggle = {
     attach: function (context, settings) {
-        $('.field-name-ding-periodical-issues .ding-periodical-fold').click(function(){
-	    $(this).parent('.ding-periodical-foldable').children('.item-list').toggle();
-        });
+      $('.field-name-ding-periodical-issues .ding-periodical-fold', context).click(function(){
+	      $(this).siblings('.item-list').toggle();
+      });
     }
   }    
 }(jQuery));
